e963ea8037cb77ea2bb840917ea43d0b1906214e
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-18 20:57:58 -0300

    chore: added theme provider and added custom files in tsconfig to provide types

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+
+import Header from "./components/Header";
+import Workflow from "./components/Workflow";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { LIGHT } from "./styles/themes/light";
+
+export default function App () {
+	return (
+		<ThemeProvider theme={LIGHT}>
+			<Header />
+			<Workflow />
+			<GlobalStyles />
+		</ThemeProvider>
+	);
+}
