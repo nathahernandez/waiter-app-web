5c97cd5478c1ab39ef2c3dbdce21c729a5eea3b7
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-19 23:35:48 -0300

    feat: created types folder

---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+export interface OrdersType {
+	_id: string;
+	table: string;
+	status: string;
+	createdAt: string;
+	products: ProductType[];
+	__v: number;
+
+}
+
+export interface ProductsType {
+	product: ProductType;
+	quantity: number;
+	_id: string;
+}
+
+export interface ProductType {
+	_id: string;
+    name: string;
+    description: string;
+    imagePath: string;
+    price: number;
+    ingredients: IngredientsType[];
+    category: string;
+    __v: number;
+}
+
+export interface IngredientsType {
+	name: string;
+	icon: string;
+	_id: string;
+}
