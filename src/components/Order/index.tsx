b7f62dbb2edb6c1c672d3782d362f5b795517959
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-21 02:38:16 -0300

    feat: now order receive others props

--- src/components/Order/index.tsx
+++ src/components/Order/index.tsx
@@ -1,14 +1,14 @@
 import { OrdersType } from "../../types";
 import { Container } from "./styles";
 
-interface OrderProps {
+interface OrderProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	order: OrdersType
 }
 
-export default function Order ({ order  } : OrderProps) {
+export default function Order ({ order, ...props  } : OrderProps) {
 	const { table, products } = order;
 	return (
-		<Container type="button">
+		<Container {...props} type="button">
 			<strong>Mesa {table}</strong>
 			<span>{products.length} itens</span>
 		</Container>
