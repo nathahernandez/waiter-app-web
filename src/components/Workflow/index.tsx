140750398a85b676c3613aad650664568cf6ec5c
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-21 02:34:20 -0300

    fix: fix to prevent infinite loop in get request

--- src/components/Workflow/index.tsx
+++ src/components/Workflow/index.tsx
@@ -12,7 +12,7 @@ export default function Workflow () {
 
 	useEffect(() => {
 		api.get("/orders").then(res => setOrdersData(res.data));
-	},[ordersData]);
+	},[]);
 
 	const {
 		statusDone,
--- src/components/Workflow/styles.ts
+++ src/components/Workflow/styles.ts
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
+	width: 100%;
 	margin-top: 40px;
 	display: flex;
 	justify-content: center;
 	align-items: center;
+
 `;
 
 export const Content = styled.div`
-	width: 100%;
 	max-width: 1216px;
 	display: flex;
 	justify-content: space-between;
 	align-items: flex-start;
+	@media (max-width: 1024px){
+		flex-direction: column;
+	}
 `;
